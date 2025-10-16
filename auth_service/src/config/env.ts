37bf3c6e81e8202ac6dd9d630c3a5b13e098a69e
GabsIel <gabrieldasnevespinheiro@gmail.com>
2025-10-15 22:18:27 -0300

    JWT env mapping

--- auth_service/src/config/env.ts
+++ auth_service/src/config/env.ts
@@ -5,4 +5,5 @@ config();
 export const env = {
   PORT: process.env.API_PORT || 3000,
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/",
+  JWT_SECRET: process.env.JWT_SECRET || "",
 };
